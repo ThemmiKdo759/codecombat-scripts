12748f9104b64d96bcd8d074a4f2511c864fe33e
Nicolas Kaiser <nikai@nikai.net>
2016-08-26 22:02:30 +0200

    add Seek And Hide
    Lua

---
+++ forest/seek-and-hide.lua
@@ -0,0 +1,35 @@
+-- Gather 4 lightstones to defeat the Brawler.
+-- Move along the outside of the camp, gathering the lightstones.
+
+-- The function should check for the item and move to hide.
+-- function checkTakeHide(item)
+--    if item then
+--        -- The item is here, take it.
+--        hero:moveXY(item.pos.x, item.pos.y)
+--        -- Then hide. Move to the center of the camp.
+--        hero:moveXY(40, 34)
+--    end
+-- end
+
+-- Move to the right mark, then to the left one.
+loop
+    -- Move to the top right mark.
+    hero:moveXY(68, 56)
+    -- Search for a stone there.
+    local stone = hero:findNearest(hero:findItems())
+    -- Call the function 'checkTakeHide' with the argument 'stone'.
+    -- checkTakeHide(stone)    
+    if stone then
+        hero:moveXY(stone.pos.x, stone.pos.y)
+        -- Then hide. Move to the center of the camp.
+        hero:moveXY(40, 34)
+    end
+    -- Move to the top left mark, search for a stone, and call the function.
+    hero:moveXY(12, 56)
+    stone = hero:findNearest(hero:findItems())
+    if stone then
+        hero:moveXY(stone.pos.x, stone.pos.y)
+        -- Then hide. Move to the center of the camp.
+        hero:moveXY(40, 34)
+    end
+end
