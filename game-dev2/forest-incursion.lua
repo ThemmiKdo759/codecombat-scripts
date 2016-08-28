5e3769b4330133b7b69c80789eed45c9da2073ef
Nicolas Kaiser <nikai@nikai.net>
2016-08-28 22:41:48 +0200

    add Forest Incursion
    Lua

---
+++ game-dev2/forest-incursion.lua
@@ -0,0 +1,16 @@
+-- Okar needs to stomp out these annoying little munchkins!
+-- Unfortunately he is slow, and his attacks do little damage.
+-- Fortunately, as a game developer, you have full control over the world!
+-- Set Okar's properties to buff him up for ogre slaying!
+
+game:addDefeatGoal()
+game:addSurviveGoal()
+local hero = game:spawnHeroXY(12, 10)
+-- Increase the hero's maxSpeed, so he runs faster.
+hero.maxSpeed = 25
+-- Increase the hero's maxHealth, so he lasts longer.
+hero.maxHealth = 999
+-- Increase the hero's health, so he can actually take the hits.
+hero.health = 999
+-- Increase the hero's attackDamage, so he can quickly kill the ogres.
+hero.attackDamage = 123
