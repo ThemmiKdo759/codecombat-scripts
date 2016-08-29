1c228842a6110f625ee2eca74a00ce7dd6f8fac2
Nicolas Kaiser <nikai@nikai.net>
2016-08-29 23:21:13 +0200

    add Buddy's Name
    Lua

---
+++ forest/buddys-name.lua
@@ -0,0 +1,16 @@
+-- We need to know the name of our new pet.
+
+-- This function is using as a callback for a pet.
+local function sayName(event)
+    pet:say("Meow Purr Meow")
+    pet:say("Purr Purr")
+    pet:say("Meow")
+    pet:say("Meow")
+    pet:say("Meow Purr Meow Meow")
+end
+
+-- Use "pet.on" method to assign "sayName" function as the event handler for "hear".
+pet:on("hear", sayName)
+
+hero:say("What's you name, buddy?")
+hero:say("Could you repeat it?")
