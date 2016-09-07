76a50be9fe2625bcdeb9b98077fd6be81a44ab1b
Nicolas Kaiser <nikai@nikai.net>
2016-09-07 20:25:28 +0200

    add Lost in the Stacks
    Lua

---
+++ dungeon/lost-in-the-stacks.lua
@@ -0,0 +1,8 @@
+-- Loops repeat a block of code multiple times.
+
+while true do
+    hero:moveRight() -- ∆ INDENT this line
+    hero:moveUp() -- ∆ INDENT this line
+    hero:moveLeft() -- ∆ INDENT this line
+    hero:moveDown() -- ∆ INDENT this line
+end
