21a9c757dc728e0560ab4c6350711c0f8468adbf
Nicolas Kaiser <nikai@nikai.net>
2016-09-10 21:27:20 +0200

    add No Pain No Gain
    Lua

---
+++ desert/no-pain-no-gain.lua
@@ -0,0 +1,9 @@
+-- Run while you can, but watch your health.
+
+-- While hero's health greater than 200: 
+while hero.health > 200 do
+    hero:moveXY(48, 24)
+    hero:moveXY(16, 24)
+end
+-- Move to Okar.
+hero:moveXY(32, 40)
