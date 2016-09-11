27bd28946f4bd26421516eef62989efd1b08470e
Nicolas Kaiser <nikai@nikai.net>
2016-09-11 13:12:39 +0200

    more The Agrippa Defense
    Lua

---
+++ forest/the-agrippa-defense-b.lua
@@ -0,0 +1,16 @@
+while true do
+    local e = self:findNearest(self:findEnemies())
+        -- Find the distance to the enemy with distanceTo.
+        -- If the distance is less than 5 meters...
+            -- ... if "cleave" is ready, cleave!
+            -- ... else, just attack.
+    if e then
+        if hero:distanceTo(e) < 5 and hero:isReady("cleave") then
+            hero:cleave(e)
+        elseif hero:isReady("bash") then
+            hero:bash(e)
+        else
+            hero:attack(e)
+        end
+    end
+end
