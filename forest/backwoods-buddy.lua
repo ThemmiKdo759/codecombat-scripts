a9f63da2636a2969c67d09c6a4dcd8aea7cb0a8f
Nicolas Kaiser <nikai@nikai.net>
2016-09-20 16:19:21 +0200

    add Backwoods Buddy
    Lua

---
+++ forest/backwoods-buddy.lua
@@ -0,0 +1,12 @@
+-- You now have a pet!
+
+local function speak(event)
+    -- Your pet should respond with pet.say()
+    pet:say("Mooh!")
+end
+
+-- This tells your pet to run the speak() function when she hears something
+pet:on("hear", speak)
+
+-- Talk to your pet!
+hero:say("Hello Kitty")
