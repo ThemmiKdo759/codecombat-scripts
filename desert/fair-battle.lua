7210a5f1bdfe5f506f24d16b43a55e9c97a1a8e3
Nicolas Kaiser <nikai@nikai.net>
2016-10-04 15:12:07 +0200

    add Fair Battle
    Lua

---
+++ desert/fair-battle.lua
@@ -0,0 +1,22 @@
+-- Today is the day for a fair battle!
+-- Wait until the total health of your soldiers is greater than ogre's.
+-- Don't attack while your soldiers don't have the advantage.
+
+-- Write a function to return the sum of health of an array:
+local function health(as)
+    local r = 0
+    for i = 1, #as do
+        r = r + as[i].health
+    end
+    return r
+end
+
+while true do
+    local fs = hero.findFriends()
+    local es = hero.findEnemies()
+    -- Calculate and compare the total health of your soldiers and the ogres.
+    -- Say "Attack" when you are ready.
+    if health(fs) > health(es) then
+        hero:say("Attack")
+    end
+end
