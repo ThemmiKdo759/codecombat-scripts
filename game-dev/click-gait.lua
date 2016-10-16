171c99011c5af95247781d8c5db553537ac2eb9c
Nicolas Kaiser <nikai@nikai.net>
2016-10-16 22:38:45 +0200

    add Over The Garden Wall, Click Gait
    Lua

---
+++ game-dev/click-gait.lua
@@ -0,0 +1,6 @@
+-- Move to all the red X marks to win.
+
+game:spawnHeroXY(5, 8)
+game:addMoveGoalXY(10, 18)
+game:addMoveGoalXY(10, 38)
+game:addMoveGoalXY(48, 41)
---
+++ game-dev/over-the-garden-wall.lua
@@ -0,0 +1,5 @@
+-- Use game.spawnXY() to build a fence around the farm!
+
+for i = 16, 44, 4 do
+    game:spawnXY("fence", 39, i)
+end
