66238943c211b4628fb53aa081f464b026ae7914
Nicolas Kaiser <nikai@nikai.net>
2016-10-18 19:01:46 +0200

    add Ranger Danger
    Lua

---
+++ game-dev2/ranger-danger.lua
@@ -0,0 +1,7 @@
+-- Archers are powerful ranged units!
+-- While soldiers have health, archers provide damage.
+
+game:spawnXY("soldier", 20, 40)
+game:spawnXY("archer", 10, 50)
+game:spawnXY("archer", 10, 40)
+game:spawnXY("archer", 10, 30)
